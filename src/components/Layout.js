26674066cdbf0e1e3771d1e66f79716bdb55f10e
myztajay <myztajay@hotmail.com>
2017-10-30 18:43:49 -0400

    Added socket client connection on Layout component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,8 +1,30 @@
+/*jshint esversion: 6 */
 import React, { Component } from 'react';
+import io from 'socket.io-client';
 
+const socketURL = 'localhost:4000';
 class Layout extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      socket:null
+    };
+  }
+  
+  componentWillMount(){
+    this.initSocket();
+  }
+  
+  initSocket = () => {
+    const socket = io(socketURL)
+    socket.on('connect', ()=>{
+      console.log('You are connected');
+    })
+    this.setState({socket})
+  }
+  
   render(){
-    const { title } = this.props
+    const { title } = this.props;
     return(
       <div className='container'>
         {title}
