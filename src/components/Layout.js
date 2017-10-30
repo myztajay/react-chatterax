a2c4feb026b8dc66eb49e5156ef5992b9e030a71
myztajay <myztajay@hotmail.com>
2017-10-30 19:25:34 -0400

    Added factories file with methods

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,16 @@
 /*jshint esversion: 6 */
 import React, { Component } from 'react';
 import io from 'socket.io-client';
+import { USER_CONNECTED } from '../Events';
 
 const socketURL = 'localhost:4000';
+
 class Layout extends Component{
   constructor(props){
     super(props);
     this.state = {
-      socket:null
+      socket:null,
+      user:null
     };
   }
   
@@ -21,6 +24,19 @@ class Layout extends Component{
       console.log('You are connected');
     })
     this.setState({socket})
+  }
+  
+  setUser = (user)=>{
+    const { socket } = this.state
+    socket.emit('USER_CONNECTED', user);
+    this.setState({ user })
+  }
+  
+  logout =()=>{
+    const { socket } = this.state
+    socket.emit('LOGOUT');
+    this.setState({ user:null });
+    
   }
   
   render(){
